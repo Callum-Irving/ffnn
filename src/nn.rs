57af322dfea0d4defaa2b9549a3920280894a0c9
Callum Irving <callum.irving@protonmail.com>
2022-02-20 16:34:19 -0500

    expand network functionality

--- src/nn.rs
+++ src/nn.rs
@@ -2,12 +2,20 @@ use nalgebra::DMatrix;
 
 struct Layer {
     weights: DMatrix<f32>,
+    activation: fn(f32) -> f32,
+}
+
+impl Layer {
+    pub fn eval(&self, inputs: Vec<f32>) -> Vec<f32> {
+        assert_eq!(self.weights.ncols(), inputs.len());
+        todo!();
+    }
 }
 
 struct Network {
     inputs: usize,
     hidden: Vec<Layer>,
-    outputs: DMatrix<f32>,
+    outputs: Layer,
 }
 
 impl Network {
@@ -24,9 +32,29 @@ impl Network {
     }
 }
 
+struct NetBuilder {
+    layers: Vec<Layer>,
+}
+
+impl NetBuilder {}
+
+// TODO: Make Layer and Network generics
+
+// TODO: Helper function that converts dataset ( Vec<Vec<f32>> ) into a DMatrix<f32?
+
 // Idea for builder pattern:
 //
 // NetBuilder::new()
 //      .layer(5, Activation:Relu)
 //      .layer(2, Activation::Sigmoid)
 //      .outputs(5, Activation::SoftMax);
+//
+// or:
+//
+// NetBuilder::new()
+//      .layer(5)
+//      .relu()
+//      .layer(2)
+//      .sigmoid()
+//      .outputs(5)
+//      .softmax()
