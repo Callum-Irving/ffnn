64d519b571f116e50afdb562711e32b671dc5c24
Callum Irving <callum.irving@protonmail.com>
2022-02-19 22:25:46 -0500

    use nalgebra library for matrix math

--- src/nn.rs
+++ src/nn.rs
@@ -1,7 +1,32 @@
+use nalgebra::DMatrix;
+
 struct Layer {
-    weights: Vec<f32>,
+    weights: DMatrix<f32>,
 }
 
 struct Network {
-    layers: Vec<Layer>,
+    inputs: usize,
+    hidden: Vec<Layer>,
+    outputs: DMatrix<f32>,
 }
+
+impl Network {
+    pub fn predict(&self, inputs: Vec<f32>) -> Vec<f32> {
+        assert_eq!(self.inputs, inputs.len());
+        todo!();
+    }
+
+    pub fn train(&mut self, dataset: DMatrix<f32>) {
+        // Number of columns in dataset should match length of inputs
+        assert_eq!(dataset.ncols(), self.inputs);
+
+        todo!();
+    }
+}
+
+// Idea for builder pattern:
+//
+// NetBuilder::new()
+//      .layer(5, Activation:Relu)
+//      .layer(2, Activation::Sigmoid)
+//      .outputs(5, Activation::SoftMax);
