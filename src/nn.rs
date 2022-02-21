3559c74fdff330ca9e9844293b9c4066e4f74afd
Callum Irving <callum.irving@protonmail.com>
2022-02-21 18:14:53 -0500

    work on builder pattern for constructing network

--- src/nn.rs
+++ src/nn.rs
@@ -6,27 +6,43 @@ struct Layer {
 }
 
 impl Layer {
+    pub fn new(nodes: usize, activation: Option<fn(f32) -> f32>) -> Self {
+        todo!();
+    }
+
     pub fn eval(&self, inputs: Vec<f32>) -> Vec<f32> {
         assert_eq!(self.weights.ncols(), inputs.len());
         todo!();
     }
+
+    pub fn len(&self) -> usize {
+        self.weights.ncols()
+    }
 }
 
 struct Network {
-    inputs: usize,
+    inputs: Layer,
     hidden: Vec<Layer>,
     outputs: Layer,
 }
 
 impl Network {
+    pub fn new(inputs: usize, hidden: Vec<Layer>, outputs: Layer) -> Self {
+        Network {
+            inputs: Layer::new(inputs, None),
+            hidden,
+            outputs,
+        }
+    }
+
     pub fn predict(&self, inputs: Vec<f32>) -> Vec<f32> {
-        assert_eq!(self.inputs, inputs.len());
+        assert_eq!(self.inputs.len(), inputs.len());
         todo!();
     }
 
     pub fn train(&mut self, dataset: DMatrix<f32>) {
         // Number of columns in dataset should match length of inputs
-        assert_eq!(dataset.ncols(), self.inputs);
+        assert_eq!(dataset.ncols(), self.inputs.len());
 
         todo!();
     }
@@ -36,7 +52,15 @@ struct NetBuilder {
     layers: Vec<Layer>,
 }
 
-impl NetBuilder {}
+impl NetBuilder {
+    pub fn new() -> Self {
+        NetBuilder { layers: vec![] }
+    }
+
+    pub fn layer(&mut self, nodes: usize, activation: Option<fn(f32) -> f32>) {
+        self.layers.push(Layer::new(nodes, activation));
+    }
+}
 
 // TODO: Make Layer and Network generics
 
