59f475877b45ed79e4425bc72ac08bba435e2642
Callum Irving <callum.irving@protonmail.com>
2022-03-10 23:09:51 -0500

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
-//! A simple feedforward neural network library.
+//! # ffnn
+//!
+//! **ffnn** is meant to be a very simple feedforward neural network library. The
+//! goal is to make creating and training feedforward neural networks as easy
+//! as possible.
+//!
+//! # Getting Started
+//!
+//! Make a neural network using the [NetBuilder] struct.
+//!
+//! ```
+//! let net = NetBuilder::new(3).layer(5, RELU).init();
+//! ```
+//!
+//! Making predictions on a set of inputs is as simple as:
+//!
+//! ```
+//! let result = net.predict(dvector![0.0, 0.0, 0.0]);
+//! ```
 
 #![warn(missing_docs)]
 
